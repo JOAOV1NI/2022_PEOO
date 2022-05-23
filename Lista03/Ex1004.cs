5dd8cafa94a6c641727f1821d63bb287cb9f3ec0
JOAOV1NI <105991633+JOAOV1NI@users.noreply.github.com>
2022-05-22 21:08:29 -0300

    Create Ex1004.cs

---
+++ Lista03/Ex1004.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Program {
+
+  public static void Main(string[] args) {
+   
+    {
+    int a = Convert.ToInt16(Console.ReadLine());
+    int b = Convert.ToInt16(Console.ReadLine());
+    int PROD = a * b;
+    
+    Console.WriteLine($"PROD = {PROD}");
+    
+    Console.ReadKey();
+    }
+    
+    }
+
+}
